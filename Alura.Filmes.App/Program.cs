a31ba5ddf16ffddc85601c8abd7c51a458f6ffce
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-09-04 17:57:41 -0300

    Get Shadow Property in the DataBase

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -1,7 +1,9 @@
 ﻿using Alura.Filmes.App.Dados;
 using Alura.Filmes.App.Extensions;
 using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
 using System;
+using System.Linq;
 
 namespace Alura.Filmes.App
 {
@@ -13,26 +15,21 @@ namespace Alura.Filmes.App
             {
                 contexto.LogSQLToConsole();
 
-                Ator ator = new Ator();
-                ator.PrimeiroNome = "Sthevan";
-                ator.UltimoNome = "Bello";
-                //contexto.Entry(ator).Property("last_update").CurrentValue = DateTime.Now;
+                var atores = contexto.Atores.OrderByDescending(a => EF.Property<DateTime>(a, "last_update")).Take(10);
+                
+                ListaAtores(atores, contexto);
 
-                contexto.Atores.Add(ator);
-                contexto.SaveChanges();
-
-
-                ListAtores(contexto);
             }
             Console.ReadKey();
         }
 
-        private static void ListAtores(AluraFilmesContexto contexto)
+        private static void ListaAtores(IQueryable<Ator> atores, AluraFilmesContexto contexto)
         {
-            foreach (var ator in contexto.Atores)
+            foreach (var ator in atores)
             {
-                Console.WriteLine(ator);
+                Console.WriteLine($"{ator} - {contexto.Entry(ator).Property("last_update").CurrentValue}");
             }
         }
+
     }
 }
