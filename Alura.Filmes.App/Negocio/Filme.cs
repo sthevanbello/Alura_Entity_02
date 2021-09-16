4236084b5b96b3ee0201603e8df8e7b7ac0bb090
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-09-16 19:55:11 -0300

    Created enumerable for to use rating

--- Alura.Filmes.App/Negocio/Filme.cs
+++ Alura.Filmes.App/Negocio/Filme.cs
@@ -8,13 +8,13 @@ namespace Alura.Filmes.App.Negocio
         public string Titulo { get; set; }
         public string Descricao { get; set; }
         public short Duracao { get; set; }
-        public string Classificacao { get; set; }
+        public ClassificacaoIndicativa Classificacao { get; set; }
         public string AnoLancamento { get; set; }
         public IList<FilmeAtor> Atores { get; set; }
         public IList<FilmeCategoria> Categoria { get; set; }
         public Idioma IdiomaFalado { get; set; }
         public Idioma IdiomaOriginal { get; set; }
-
+        
 
 
         public Filme()
