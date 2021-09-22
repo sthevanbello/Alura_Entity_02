313b2005c52bef9876f066deccef4a4acf055b2d
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-09-22 19:37:00 -0300

    Created class base ClienteConfiguration to Context Configuration

---
+++ Alura.Filmes.App/Dados/ClienteConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Alura.Filmes.App.Negocio;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+
+namespace Alura.Filmes.App.Dados
+{
+    public class ClienteConfiguration : PessoaConfiguration<Cliente>
+    {
+        public override void Configure(EntityTypeBuilder<Cliente> builder)
+        {
+            base.Configure(builder);
+
+            builder.ToTable("customer");
+            builder.Property(c => c.Id).HasColumnName("customer_id").HasColumnType("tinyint");
+            builder.Property<DateTime>("create_date").HasColumnType("datetime").HasDefaultValueSql("getdate()").IsRequired();
+        }
+    }
+}
