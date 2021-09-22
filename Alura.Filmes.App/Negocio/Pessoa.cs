d3049d3767413925e176a8591c63ff5d38fa1651
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-09-22 19:34:43 -0300

    Created class base Pessoa for herintance

---
+++ Alura.Filmes.App/Negocio/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Negocio
+{
+    abstract public class Pessoa
+    {
+        public byte Id { get; set; }
+        public string PrimeiroNome { get; set; }
+        public string UltimoNome { get; set; }
+        public string Email { get; set; }
+        public bool Ativo { get; set; }
+
+        public override string ToString()
+        {
+            var tipo = this.GetType().Name;
+
+            return $"{tipo} - Id: ({Id}) - Nome: {PrimeiroNome} {UltimoNome} - Email: {Email} - Ativo: {Ativo}";
+        }
+    }
+}
