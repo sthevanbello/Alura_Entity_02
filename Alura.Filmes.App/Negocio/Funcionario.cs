e24bc1cb76d15817771a6a5ea819f8c3c269fe56
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-09-22 19:36:20 -0300

    Class Funcionario to staff table in the SQL

---
+++ Alura.Filmes.App/Negocio/Funcionario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Filmes.App.Negocio
+{
+    public class Funcionario : Pessoa
+    {
+
+        public string Login { get; set; }
+        public string Senha { get; set; }
+
+    }
+}
