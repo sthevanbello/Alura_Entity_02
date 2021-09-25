b56ff6f5b012cd3f9a316c5e10b119f020285a9d
Sthevan Bello Alves <sthevan.alves@gmail.com>
2021-09-24 21:59:25 -0300

    Query using FromSQL

--- Alura.Filmes.App/Program.cs
+++ Alura.Filmes.App/Program.cs
@@ -14,14 +14,10 @@ namespace Alura.Filmes.App
         {
             using (var contexto = new AluraFilmesContexto())
             {
+                // Usando FromSql com uma view 'top5_most_starred_actors'
                 var sql = @"SELECT A.*
                             FROM actor A
-                                INNER JOIN 
-                            (SELECT TOP 5 A.actor_id, COUNT(*) AS TOTAL
-                            FROM actor AS A
-                                INNER JOIN  film_actor AS FA ON FA.actor_id = A.actor_id
-                            GROUP BY A.actor_id
-                            ORDER BY TOTAL DESC) FILMES ON FILMES.actor_id = A.actor_id";
+                                INNER JOIN top5_most_starred_actors FILMES ON FILMES.actor_id = A.actor_id";
 
                 contexto.LogSQLToConsole();
 
